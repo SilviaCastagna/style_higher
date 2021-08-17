cb90ab1b33e765fe8301a1ed9dd862a99a4df5f3
gabriel pretto <gabrielpretto@yahoo.com.br>
2021-08-17 17:57:30 +0200

    creating a new item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-
+  before_action :authenticate_user!, except: [:index, :show]
   def show
     @item = Item.find(params[:id])
   end
@@ -7,5 +7,27 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    # @item = Item.new(params[:item])
+    @item = Item.new(item_params)
+    @item.user = current_user
+    if @item.save
+      redirect_to items_panth(@item)
+    else
+    raise
+    end
+  end
+
+  private
+
+  def item_params
+      params.require(:item).permit(:title, :description, :brand)
+  end
+
 
 end
