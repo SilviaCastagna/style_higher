b95bb5b1fe969eeba34b7059ff9287bf22271fe0
Yorick ten Feld <yorick.tenfeld@gmail.com>
2021-08-17 15:09:48 +0200

    routes and controller for bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,15 @@
 class BookingsController < ApplicationController
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.save
+  end
+
+
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:title, :description, :brand)
+  end
 end
