d4de0d877ea9ffebbc0968d15c640f1233881327
Silvia Castagna <silvia.castagna82@gmail.com>
2021-08-17 11:01:41 +0200

    models item, booking, user with validations and associations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_many :bookings, dependent: :destroy
+  validates :title, presence: true
+  validates :description, length: { minimum: 20 }, allow_blank: false
 end
