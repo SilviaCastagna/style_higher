c6f95a6ff1b5627cf855d35d2952edc1010400ba
Yorick ten Feld <yorick.tenfeld@gmail.com>
2021-08-20 15:08:53 +0200

    changed validations, added signin flash

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,6 @@ class Item < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   validates :title, presence: true
-  validates :description, length: { minimum: 20 }, allow_blank: false
+  validates :description, length: { minimum: 1 }, allow_blank: false
   has_one_attached :photo
 end
